1ae468bf6f389ddda79f66564ea4638cb070de6d
j2n <manu.j2n@gmail.com>
2018-05-21 11:42:50 +0200

    suppression de remarque

--- Pi_Timer.ino
+++ Pi_Timer.ino
@@ -18,7 +18,6 @@ void loop()
 {
   // here is where you'd put code that needs to be running all the time.
 
-  // check to see if it's time to blink the LED; that is, if the 
   // difference between the current time and last time you blinked 
   // the LED is bigger than the interval at which you want to 
   // blink the LED.
