fe287ca303588e2287817bb9a1d867315bcc83c3
j2n <manu.j2n@gmail.com>
2018-05-21 12:09:18 +0200

    suppression d'une ligne

--- Pi_Timer/Pi_Timer.ino
+++ Pi_Timer/Pi_Timer.ino
@@ -10,7 +10,6 @@ long previousMillis = 0;        // will store last time LED was updated
 long interval = 1000;           // interval at which to blink (milliseconds)
 
 void setup() {
-  // set the digital pin as output:
   pinMode(ledPin, OUTPUT);      
 }
 
