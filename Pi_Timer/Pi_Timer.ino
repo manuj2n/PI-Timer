5dd03754c3864fc4bf177852835c462f2ca75aa5
j2n <manu.j2n@gmail.com>
2018-05-26 11:05:58 +0200

    modif important, allume la led avec un retour chario

--- Pi_Timer/Pi_Timer.ino
+++ Pi_Timer/Pi_Timer.ino
@@ -1,34 +1,56 @@
 
-const int cmdPiPin =  2;      // the number of the LED pin
 
-// Variables will change:
-int piState = LOW;             // ledState used to set the LED
-long previousMillis = 0;        // will store last time LED was updated
+const int outUSB =  2;      // Sortie commande mosfet usb raspberry
+const int outShutdown = 4;    // ligue IO vers raspberry
+const int outLed = 5;          // led debug
+const int inRaspberry = 3;     //ligne IO venant du raspberry
 
-// the follow variables is a long because the time, measured in miliseconds,
-// will quickly become a bigger number than can be stored in an int.
-long interval = 20000;           // interval at which to blink (milliseconds)
+String bufferString = "";         // a string to hold incoming data
+boolean bufferFull = false;  // whether the string is complete
+
+int piState = LOW;             // etat raspberry
+unsigned long tempsVeille = 300;
 
 void setup() {
-  pinMode(cmdPiPin, OUTPUT);      
+  pinMode(outUSB, OUTPUT);
+  digitalWrite(outUSB, LOW);
+  pinMode(outShutdown, OUTPUT);
+  digitalWrite(outShutdown, LOW);
+  pinMode(outLed, OUTPUT);
+  digitalWrite(outLed, LOW);
+  pinMode(inRaspberry, INPUT);
+  // initialize serial:
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+  bufferString.reserve(200);
+  
 }
 
 void loop()
 {
+  // if the PI is off turn it on and vice-versa:
+  if (piState == LOW){
+    piState = HIGH;
+  } else {
+    piState = LOW;      
+  }
+  
+  if (bufferFull) {
+    digitalWrite(outLed,HIGH);
+    delay(1000);
+    digitalWrite(outLed,LOW);  
+    bufferString = "";
+    bufferFull = false;
+  }
+   
+}
 
-  unsigned long currentMillis = millis();
- 
-  if(currentMillis - previousMillis > (interval)) {
-    // enregistre le compteur avec l'interval prevu'
-    previousMillis = currentMillis;
-
-    // if the PI is off turn it on and vice-versa:
-    if (piState == LOW)
-      piState = HIGH;
-    else
-      piState = LOW;
-
-    // set the PI with the ledState of the variable:
-    digitalWrite(cmdPiPin, piState);
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read(); 
+    bufferString += inChar;
+    if (inChar == '\n') {
+      bufferFull = true;
+    } 
   }
 }
